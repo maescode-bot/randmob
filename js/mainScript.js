325197d702735ef4d73521e386e805378256927d
Abel Olayinka <56263750+maescode-bot@users.noreply.github.com>
2020-07-13 14:23:07 +0100

    Add files via upload

---
+++ js/mainScript.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$(document).ready(function(){
+
+  // jQuery methods go here...
+ $("#vof").hide();
+	
+	
+ $("#car_owner").click(function(){
+ $("#vof").toggle();
+ 
+
+	
+});
+
+});
+
